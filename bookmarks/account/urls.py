c8699b72134279033d1de10821c057435f1f749b
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-13 14:54:22 +0530

    Adding the login template for the django custom login form .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,13 +1,13 @@
 # view for the account app
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 # pattern for the user login
 urlpatterns = [
     #path('login/',views.user_login,name='login'),
     # time for using the django auth views
-    path('login/',auth_views.LoginViews.as_views(),name="login"),
-    path('logout/',auth_views.LogoutViews.as_views(),name="logout"),
+    path('login/',auth_views.LoginView.as_view(),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(),name="logout"),
 ]
