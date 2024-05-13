27497e613f1fc72f5b16e597c5441060e0850040
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-13 14:42:34 +0530

    Adding the login,logout view for the django .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,13 @@
 # view for the account app
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 # pattern for the user login
 urlpatterns = [
-    path('login/',views.user_login,name='login'),
+    #path('login/',views.user_login,name='login'),
+    # time for using the django auth views
+    path('login/',auth_views.LoginViews.as_views(),name="login"),
+    path('logout/',auth_views.LogoutViews.as_views(),name="logout"),
 ]
