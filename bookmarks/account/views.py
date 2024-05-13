a507985faa111ccbd6c09bf50fd95d4b84cce611
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-13 12:47:36 +0530

    Adding user login view for the user .

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login,authenticate
+from .forms import LoginForms
 
 # Create your views here.
+
+# user login view
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForms(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username = cd["username"],
+                                password=cd["password"])
+            
+            if user is not None:
+                if user.is_active:
+                    return HttpResponse("Authenticate Successfully.")
+                else:
+                    return HttpResponse("Disabled Account.")
+            else:
+                return HttpResponse("Invalid Login.")
+            
+    else:
+        form = LoginForms()
+    return render(
+        request,
+        'account/login.html',
+        {
+            'form' : form
+        }
+    )
