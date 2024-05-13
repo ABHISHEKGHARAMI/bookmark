c0d91041685f6916bb8011b59f50f8ebc99eaeec
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-13 15:51:06 +0530

    Adding the redirection for the repo .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,6 +8,9 @@ from . import views
 urlpatterns = [
     #path('login/',views.user_login,name='login'),
     # time for using the django auth views
-    path('login/',auth_views.LoginView.as_view(),name="login"),
-    path('logout/',auth_views.LogoutView.as_view(),name="logout"),
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+    
+    # adding the dashboard
+    path('',views.dashboard,name='dashboard'),
 ]
