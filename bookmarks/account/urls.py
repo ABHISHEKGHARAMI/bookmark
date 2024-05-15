813ee57d18bd6dc4e5b0f545fbb579ef025e29a9
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-15 10:53:41 +0530

    Adding the password view for user help .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     
+    #  adding the password change for the user
+    path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    
+    # adding the password change done
+    path('password-change/done/',auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+    
     # adding the dashboard
     path('',views.dashboard,name='dashboard'),
 ]
