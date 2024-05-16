d31a0973d5be65ba634ee68f49a6c428b0a0aba9
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-16 22:09:08 +0530

    Added the user registration  view for the new user now time to go for the template .

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -49,4 +49,34 @@ def dashboard(request):
     
 # register  view for new user
 def register(request):
-    pass
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.method)
+        # checking the user form is valid
+        if user_form.is_valid :
+            # creating the user but dont commit the user
+            new_user = user_form.save(commit=False)
+            # check the password for the user
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            
+            # saving the password
+            new_user.save()
+            
+            
+            return render(
+                request,
+                "account/register_done.html",
+                {
+                    'new_user' : new_user
+                }
+            )
+    else:
+        user_form = UserRegistrationForm()
+        
+    # render the registration form
+    return render(request,
+                  "account/register.html",
+                  {
+                      'user_form' : user_form
+                  })
