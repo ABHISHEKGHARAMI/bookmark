00f9afa428b4ae7c729c073cf433b75526809bdb
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-17 09:48:17 +0530

    mod .

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+# have to add more information along with django user model so we made addition model
+# which will work one-to-one along with django user model
+
+class 
