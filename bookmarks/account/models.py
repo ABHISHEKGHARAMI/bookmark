ed64bba6f928dee3c4940d9a208a953e11821e3d
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-17 10:13:30 +0530

    Added the Profile model .

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -6,4 +6,16 @@ from django.conf import settings
 # have to add more information along with django user model so we made addition model
 # which will work one-to-one along with django user model
 
-class 
+class Profile(models.Model):
+    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    
+    date_of_birth = models.DateField(blank=True,null=True)
+    
+    image = models.ImageField(upload_to='users/%Y/%m/%d/',
+                              blank=True)
+    
+    
+    def __str__(self):
+        return f'Profile of {self.user.username}'
