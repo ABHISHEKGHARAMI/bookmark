0843cf42eee8aaf80d2f7de5064aba67200bb0f0
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-17 12:10:15 +0530

    Checking the user email for the duplication .

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -33,6 +33,15 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('password does not match!!')
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
+
+
+
     
 class UserEditForm(forms.ModelForm):
     class Meta:
