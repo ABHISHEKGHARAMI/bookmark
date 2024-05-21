9e42a5a96bf830561fd743a5868a1e7b48e6a7e9
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-21 10:41:23 +0530

    Registered the image model to the admin .

--- bookmarks/images/admin.py
+++ bookmarks/images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Image
 # Register your models here.
+
+#registering the image model to the project
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','image','created']
+    list_filter = ['-created']
