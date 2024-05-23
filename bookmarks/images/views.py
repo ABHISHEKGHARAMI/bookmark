4ac3aad877c3967f20da12939fd904b2c7c4587d
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-23 13:57:04 +0530

    .mod

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -68,3 +68,34 @@ def image_like(request):
     return JsonResponse({
         'status' : 'error'
     })
+    
+    
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images,8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+        
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {
+                          'section':'images',
+                          'images' : images
+                      })
+    return render(request,
+                  'images/image/list.html',
+                  {
+                      'section' : 'images',
+                      'images' : images
+                  }
+                  )
