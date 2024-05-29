d548022dd35b4d8067ebc33cb6296202bbefa767
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-29 14:21:11 +0530

    .mod

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -27,7 +27,7 @@ class Profile(models.Model):
 # model for the contact
 class Contact(models.Model):
     #here goes the property.
-    user_form = models.ForeignKey('auth.User',
+    user_from = models.ForeignKey('auth.User',
                                   related_name = 'rel_from_set',
                                   on_delete=models.CASCADE)
     
