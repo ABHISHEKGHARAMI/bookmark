af0f411a0f6a272b7d67d9f3faef6dd1aedd5188
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-29 16:00:06 +0530

    Adding Action model .

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Actions(models.Model):
+    user = models.ForeignKey('auth.User',
+                             related_name='actions',
+                             on_delete=models.CASCADE)
+    
+    verb = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering=['-created']
