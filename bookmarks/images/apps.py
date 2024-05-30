a9f55e72c10f1aa43d065ee6ea137542c85b8b58
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-30 21:25:37 +0530

    Adding the signals to the apps configurations .

--- bookmarks/images/apps.py
+++ bookmarks/images/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+    
+    
+    # importing the signal for when its populated
+    def ready(self):
+        import images.signals
